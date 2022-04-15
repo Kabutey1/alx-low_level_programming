63ddd8719a23437074f5df1e8b7d9b4de96d5a2a
kabutey1 <93051065+Kabutey1@users.noreply.github.com>
2022-04-15 22:28:00 +0000

    Delete 0x04-more_functions_nested_loops directory

--- 0x04-more_functions_nested_loops/101-print_number.c
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/Kabutey1/alx-low_level_programming/blob/main/0x04-more_functions_nested_loops/101-print_number.c</string>
-</dict>
-</plist>
